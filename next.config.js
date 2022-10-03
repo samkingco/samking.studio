c1e649a1496d83dbe08a35905b05d80da84fef96
Sam King <mail@samking.co>
2022-10-03 22:18:36 +0100

    Add redirects for discord

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.com/invite/5AfbW2kMB3",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/samkingco",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
